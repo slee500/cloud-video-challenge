68418cb4e282e3301d394d9ceaaa08297d63634f
slee500 <slee500@illinois.edu>
2017-04-14 02:09:20 -0500

    added vocab

--- 04.13.train.py
+++ 04.13.train.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 
-# Today I learn how to read stuff a tfrecords file, working with frame-level data
+# Today I attempt to learn how to read stuff a tfrecords file, working with frame-level data
 # Designed to work with Python 3.5
 
 # Resources
+# https://indico.io/blog/tensorflow-data-inputs-part1-placeholders-protobufs-queues/
+
 # http://warmspringwinds.github.io/tensorflow/tf-slim/2016/12/21/tfrecords-guide/
 # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/how_tos/reading_data/fully_connected_reader.py
+
+# Frame-level features are stored as 'tensorflow.SequenceExample' protocol buffers. 
 
 filename = 'train-0.tfrecord'
 filename_queue = tf.train.string_input_producer([filename], num_epochs=1)
@@ -24,7 +28,16 @@ features = tf.parse_single_example(
           'audio': tf.FixedLenFeature([], tf.string),
       })
 
-rgb = tf.decode_raw(features['rgb'], tf.uint8)
-video_id = tf.cast(features['video_id'], tf.string)
-labels = tf.cast(features['labels'], tf.int64)
-print (labels)
+###############################################
+
+for serialized_example in tf.python_io.tf_record_iterator(filename):
+    example = tf.train.Example()
+    example.ParseFromString(serialized_example)
+
+    # traverse the Example format to get data
+    image = example.features.feature['rgb'].bytes_list.value
+    label = example.features.feature['labels'].int64_list.value
+    # do something
+    print (type(image), label)
+
+# print(tf.summary.image('frames', rgb))
