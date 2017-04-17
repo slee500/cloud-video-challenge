a37568acff76ebff5f5f5b390cb8765878de6b67
slee500 <slee500@illinois.edu>
2017-04-16 22:07:39 -0500

    read a bit of data

--- 04.16.read_data.py
+++ 04.16.read_data.py
@@ -14,13 +14,13 @@ sess = tf.InteractiveSession()
 for example in tf.python_io.tf_record_iterator(filename):
     tf_seq_example = tf.train.SequenceExample.FromString(example)
 
-    print (tf_seq_example.context.feature['video_id'].bytes_list.value[0])
-    print (len(tf_seq_example.feature_lists.feature_list['rgb'].feature))
+    print tf_seq_example.context.feature['video_id'].bytes_list.value[0]
+    print len(tf_seq_example.feature_lists.feature_list['rgb'].feature)
 
     x = tf.cast(tf.decode_raw(tf_seq_example.feature_lists.feature_list['rgb'].feature[0].
         bytes_list.value[0],tf.uint8),tf.float32).eval()
 
-    print (x)
+    print x
 
     # traverse the Example format to get data
     # image = example.features.feature['rgb'].features[0].bytes_list.value[0]
